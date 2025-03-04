4d1401111eaf982e10c59bc40fe6a35d753af89a
Anuj <anuj6353@gmail.com>
2025-03-04 19:59:58 +0530

    Update build configuration and MIME types

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-export default {
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
   base: '/',
   build: {
     rollupOptions: {
       output: {
-        format: 'es'
+        format: 'es',
+        entryFileNames: '[name].[hash].js',
+        chunkFileNames: '[name].[hash].js',
+        assetFileNames: '[name].[hash][extname]'
       }
     }
   }
-} 
+}) 
